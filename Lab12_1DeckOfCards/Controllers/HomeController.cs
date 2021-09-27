070238ad08be1b85cbde67d6e63a1075280cc144
Adam Rolain <AdamRolain@QuickenLoans.com>
2021-09-27 16:50:39 -0400

    User can start a new deck and be shown their hand of 5. Then they can keep drawing a new set of 5 until the deck runs out.

--- Lab12_1DeckOfCards/Controllers/HomeController.cs
+++ Lab12_1DeckOfCards/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Lab12_1DeckOfCards.Controllers
@@ -25,7 +26,28 @@ namespace Lab12_1DeckOfCards.Controllers
 
         public async Task<IActionResult> NewDeck()
         {
-            return Content("New Deck");
+            string domain = "https://deckofcardsapi.com";
+            string path = "api/deck/new/draw/?count=5";
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(domain);
+            var connection = await client.GetAsync(path);
+            Root result = await connection.Content.ReadAsAsync<Root>();
+
+            return View(result);
+        }
+
+        public async Task<IActionResult> Draw(string deckId)
+        {
+            string domain = "https://deckofcardsapi.com";
+            string path = $"api/deck/{deckId}/draw/?count=5";
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(domain);
+            var connection = await client.GetAsync(path);
+            Root result = await connection.Content.ReadAsAsync<Root>();
+
+            return View("NewDeck", result);
         }
 
         public IActionResult Privacy()
---
+++ Lab12_1DeckOfCards/Models/DeckOfCards.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab12_1DeckOfCards.Models
+{
+    public class Root
+    {
+        public bool success { get; set; }
+        public string deck_id { get; set; }
+        public List<Card> cards { get; set; }
+        public int remaining { get; set; }
+    }
+
+    public class Card
+    {
+        public string code { get; set; }
+        public string image { get; set; }
+        public string value { get; set; }
+        public string suit { get; set; }
+    }
+}
